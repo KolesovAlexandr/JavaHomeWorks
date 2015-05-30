9b6c2834496042e90b6222e8d3a8eb0515ec4f6a
Alexandr <KolesoAlexandr@gmail.com>
2015-05-31 01:18:48 +0300

    task pokercombination finished

--- JavaHomeWorks/src/pokercombination/PokerCombination.java
+++ JavaHomeWorks/src/pokercombination/PokerCombination.java
@@ -1,20 +1,78 @@
 package pokercombination;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class PokerCombination {
-	
-	String combination(int[] rank,int[] suit)	{
-		
-		
-		return null;
-		
-	}
-	
-	public PokerCombination() {
-		
-		
+
+	public static String combination(Card[] cards) {
+
+		boolean isThreeOfAKind = false;
+
+		int countPair = 0;
+
+		// heart, spade, diamond, club
+		int[] suitCount = new int[4];
+
+		// "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
+		int[] rankCount = new int[13];
+
+		String comb = "";
+
+		if (cards!=null && cards.length == 5) {
+			for (Card card : cards) {
+				suitCount[card.getSuit()]++;
+				rankCount[card.getRank()]++;
+			}
+			for (int sCount : suitCount) {
+				if (sCount == 5) {
+					comb = "Flush";
+					if (rankCount[0] == 1 && rankCount[12] == 1
+							&& rankCount[11] == 1 && rankCount[10] == 1
+							&& rankCount[9] == 1) {
+						comb = "Royal"+ comb;
+					}
+				}
+			}
+
+			for (int i = rankCount.length; i >= 5; i--) {
+				boolean isStraight = true;
+				for (int j = i - 5; j < i & isStraight; j++) {
+					isStraight = rankCount[j] == 1;
+				}
+				if (isStraight) {
+					comb = "Straight" + comb;
+				}
+			}
+
+			for (int rCount : rankCount) {
+				if (rCount == 4) {
+					comb = "Four of a kind";
+				} else {
+					if (rCount == 3) {
+						comb = "Three of a kind";
+						isThreeOfAKind = true;
+					}
+
+					if (rCount == 2) {
+						comb = "Pair";
+						countPair++;
+					}
+
+				}
+
+				if (countPair == 2) {
+					comb = "Two pairs";
+				}
+				if (isThreeOfAKind && countPair == 1) {
+					comb = "Full house";
+				}
+
+			}
+		}
+		if (comb == "") {
+			comb = "no combinations";
+		}
+
+		return comb;
+
 	}
 
 }
--- JavaHomeWorks/src/pokercombination/PokerCombinationTest.java
+++ JavaHomeWorks/src/pokercombination/PokerCombinationTest.java
@@ -3,10 +3,7 @@ package pokercombination;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,18 +14,113 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class PokerCombinationTest {
 	
-	private int _input;
+	private static final Card[] ROYALFLUSH = {
+		new Card(0, 0),
+		new Card(0, 12),
+		new Card(0, 11),
+		new Card(0, 10),
+		new Card(0, 9),
+		};
+	
+	private static final Card[] STRAIGHTFLUSH = {
+		new Card(0, 12),
+		new Card(0, 11),
+		new Card(0, 10),
+		new Card(0, 9),
+		new Card(0, 8),
+		};
+	private static final Card[] FOUROFKIND = {
+		new Card(0, 1),
+		new Card(1, 1),
+		new Card(2, 1),
+		new Card(3, 1),
+		new Card(0, 3),
+		};
+	private static final Card[] FULLHOUSE = {
+		new Card(0, 1),
+		new Card(1, 1),
+		new Card(2, 1),
+		new Card(0, 2),
+		new Card(1, 2),
+		};
+	private static final Card[] FLUSH = {
+		new Card(0, 1),
+		new Card(0, 2),
+		new Card(0, 3),
+		new Card(0, 4),
+		new Card(0, 7),
+		};
+	private static final Card[] STRAIGHT = {
+		new Card(0, 0),
+		new Card(1, 1),
+		new Card(2, 2),
+		new Card(3, 3),
+		new Card(0, 4),
+		};
+	private static final Card[] THREEOFAKIND = {
+		new Card(0, 3),
+		new Card(1, 3),
+		new Card(2, 3),
+		new Card(3, 7),
+		new Card(0, 9),
+		};
+	private static final Card[] TWOPAIRS = {
+		new Card(0, 1),
+		new Card(1, 1),
+		new Card(2, 2),
+		new Card(3, 2),
+		new Card(0, 0),
+		};
+	private static final Card[] ONEPAIR = {
+		new Card(0, 1),
+		new Card(1, 1),
+		new Card(2, 3),
+		new Card(3, 5),
+		new Card(0, 7),
+		};
+
+	private static final Card[] NOCOMBINATION = {
+		new Card(0, 1),
+		new Card(1, 3),
+		new Card(2, 4),
+		new Card(3, 7),
+		new Card(0, 9),
+		};
+
+	private static final Card[] ERRORLENGTH = {
+		new Card(0, 1),
+		new Card(1, 3),
+		new Card(2, 4),
+		new Card(3, 7),
+		};
+
+	private static final Card[] NULL = null;
+	
+	private Card[] _input;
 	private String _output;
 	
 	@Parameters
 	public static List<Object[]> data()	{
 		return Arrays.asList(new Object[][]	{
-			{1,1}
-		}
+			{ROYALFLUSH,"RoyalFlush"},
+			{STRAIGHTFLUSH, "StraightFlush"},
+			{FOUROFKIND, "Four of a kind"},
+			{FULLHOUSE, "Full house"},
+			{FLUSH, "Flush"},
+			{STRAIGHT, "Straight"},
+			{THREEOFAKIND, "Three of a kind"},
+			{TWOPAIRS, "Two pairs"},
+			{ONEPAIR, "Pair"},
+			{NOCOMBINATION, "no combinations"},
+			{ERRORLENGTH , "no combinations"},
+			{NULL , "no combinations"},
+			
+			
+			}
 		);
 	}
 	
-	public PokerCombinationTest(final int input, final String output) {
+	public PokerCombinationTest(final Card[] input, final String output) {
 		
 		_input = input;
 		_output = output;
@@ -38,7 +130,7 @@ public class PokerCombinationTest {
 
 	@Test
 	public void test() {
-		assertEquals(_output, _input);
+		assertEquals(_output, PokerCombination.combination(_input));
 	}
 
 }
